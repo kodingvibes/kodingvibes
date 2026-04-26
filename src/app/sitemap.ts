f77ca8eeae2661769964d071fd3519ee32239866
Madkoding <madkoding@gmail.com>
2026-04-26 12:15:23 -0400

    fix(sitemap): add force-dynamic to prevent static rendering error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // Base URL
   const baseUrl = 'https://www.kodingvibes.com'
